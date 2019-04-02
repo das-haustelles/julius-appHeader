d4668db3c26ef4ca73ff5395dd04fb38113eeae0
Julius Simtim <juliussimtim@gmail.com>
2019-04-02 16:09:39 -0700

    Cleaned up seeder.

--- database/index.js
+++ database/index.js
@@ -7,47 +7,5 @@ db.once('open', function() {
   console.log('jsDb database connected')
 })
 
-// // Settings collection
-// const settingsSchema = new mongoose.Schema({
-//   language: String,
-// 	currency: String
-// })
-// const Settings = mongoose.model('Settings', settingsSchema);
-
-// var testSetting = new Settings({
-//   language: 'English', 
-//   currency: 'USD'
-// })
-// console.log('testSetting language: ', testSetting.language)
-
-// testSetting.save(function(err) {
-//   if (err) return console.log(err);
-//   // data saved!
-// })
-
-// Signin collection
-// const signinSchema = new mongoose.Schema({
-//   email: String,
-// 	username: String,
-// 	password: String
-// })
-// const Signin = mongoose.model('Signin', signinSchema);
-
-// // Location collection
-// const locationSchema = new mongoose.Schema({
-//   country: String,
-// 	state: String,
-// 	city: String,
-// 	address: String,
-// 	badges: String
-// })
-// const Location = mongoose.model('Location', locationSchema);
-
-// // Photos collection
-// const photosSchema = new mongoose.Schema({
-//   photoUrl: String
-// })
-// const Photos = mongoose.model('Photos', photosSchema);
-
 
 module.exports = db;
